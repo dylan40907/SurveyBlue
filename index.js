f57f07feeda59b65993ec835a04f28d7c7454faf
Dylan Huang <DylanHuang@Dylans-Macbook-Pro-Computer.local>
2021-05-12 08:16:47 -0700

    2.7

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './src/testbt.js';
+import App from './src/App.js';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
